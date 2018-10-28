71d37b69d6f6212f9bbac1779564becd975697a7
Joshua Liang <joshcliang@yahoo.com>
2018-10-28 12:22:31 -0700

    Update LeetCode/Missing Number/src/Solutions/FirstSolution.java

--- LeetCode/Missing Number/src/Solutions/FirstSolution.java
+++ LeetCode/Missing Number/src/Solutions/FirstSolution.java
@@ -1,4 +1,26 @@
 package Solutions;
+import java.util.HashSet;
 
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(
+ */
 public class FirstSolution {
+
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        int missingNumber = 0;
+        for (int i = 0; i < nums.length; i++){
+            hashSet.add(nums[i]);
+        }
+        for (int i = 0; i < nums.length + 1; i++){
+            if (!hashSet.contains(i)) {
+                missingNumber = i;
+            }
+        }
+        return missingNumber;
+    }
+
+
 }
