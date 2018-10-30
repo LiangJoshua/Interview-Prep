8f26a530c1aa4e8c39628c4c80cd187154b5db66
Joshua Liang <joshcliang@yahoo.com>
2018-10-29 21:37:55 -0700

    Update LeetCode/Valid Parentheses/src/Solutions/FirstSolution.java

--- LeetCode/Valid Parentheses/src/Solutions/FirstSolution.java
+++ LeetCode/Valid Parentheses/src/Solutions/FirstSolution.java
@@ -1,4 +1,48 @@
 package Solutions;
 
+import java.util.*;
+
 public class FirstSolution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.peek() == '(') {
+
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        FirstSolution test = new FirstSolution();
+        System.out.println(test.isValid("]"));
+    }
 }
+
